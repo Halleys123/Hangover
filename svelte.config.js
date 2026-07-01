fe357de54ef382a56c8963236a8f8fc7e0efee25
Halleys123 <arnavadvik2003@gmail.com>
2026-07-01 19:50:50 +0530

    config: enable static adapter and prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
   kit: {
-    adapter: adapter(),
-  },
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    }),
+    prerender: {
+      crawl: true,
+      entries: ['*']
+    }
+  }
 };
